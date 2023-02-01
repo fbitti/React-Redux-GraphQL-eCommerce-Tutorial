9c23dd35261654cce4f6107ec212a660e1a33810
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2023-02-01 11:46:54 +0100

    downgrade the project to use React 16.13.1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Header = props => {
@@ -7,7 +8,18 @@ const Header = props => {
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="Logo" />
+          <Link to="/">
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">
+                Register
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
